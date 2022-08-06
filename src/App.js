3b94ab050a7a32783679dbfd2a673816fd03daea
jad-nasser <82750984+jad-nasser@users.noreply.github.com>
2022-08-06 19:53:15 +0200

    Optimising website performance

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import { Suspense, lazy } from "react";
-import { HashRouter as Router, Switch, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Main from "./Main";
 
 const SunglassesOnlineStore = lazy(() =>
@@ -18,38 +18,24 @@ function App() {
   return (
     <Suspense fallback={<div></div>}>
       <Router>
-        <Switch>
-          <Route path="/free-images">
-            <FreeImages />
-          </Route>
-          <Route path="/free-library">
-            <FreeLibrary />
-          </Route>
-          <Route path="/sunglasses-online-store">
-            <SunglassesOnlineStore />
-          </Route>
-          <Route path="/from-excel-to-relational-database-converter">
-            <FromExcelToRelationalDatabaseConverter />
-          </Route>
-          <Route path="/services">
-            <Main goTo="services" />
-          </Route>
-          <Route path="/about">
-            <Main goTo="about" />
-          </Route>
-          <Route path="/work">
-            <Main goTo="work" />
-          </Route>
-          <Route path="/contact">
-            <Main goTo="contact" />
-          </Route>
-          <Route path="/home">
-            <Main goTo="home" />
-          </Route>
-          <Route path="/">
-            <Main />
-          </Route>
-        </Switch>
+        <Routes>
+          <Route path="/free-images" element={<FreeImages />} />
+          <Route path="/free-library" element={<FreeLibrary />} />
+          <Route
+            path="/sunglasses-online-store"
+            element={<SunglassesOnlineStore />}
+          />
+          <Route
+            path="/from-excel-to-relational-database-converter"
+            element={<FromExcelToRelationalDatabaseConverter />}
+          />
+          <Route path="/services" element={<Main goTo="services" />} />
+          <Route path="/about" element={<Main goTo="about" />} />
+          <Route path="/work" element={<Main goTo="work" />} />
+          <Route path="/contact" element={<Main goTo="contact" />} />
+          <Route path="/home" element={<Main goTo="home" />} />
+          <Route path="/" element={<Main />} />
+        </Routes>
       </Router>
     </Suspense>
   );
