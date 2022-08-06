be4920a89e47a29d850edbe6e9fc0b2ace391be6
jad-nasser <82750984+jad-nasser@users.noreply.github.com>
2022-08-06 07:23:35 +0200

    Optimising website performance

--- src/App.js
+++ src/App.js
@@ -1,47 +1,57 @@
 import "./App.css";
+import { Suspense, lazy } from "react";
 import { HashRouter as Router, Switch, Route } from "react-router-dom";
-import SunglassesOnlineStore from "./Project/SunglassesOnlineStore/SunglassesOnlineStore";
-import FromExcelToRelationalDatabaseConverter from "./Project/FromExcelToRelationalDatabaseConverter/FromExcelToRelationalDatabaseConverter";
-import FreeLibrary from "./Project/FreeLibrary/FreeLibrary";
 import Main from "./Main";
-import FreeImages from "./Project/FreeImages/FreeImages";
+
+const SunglassesOnlineStore = lazy(() =>
+  import("./Project/SunglassesOnlineStore/SunglassesOnlineStore")
+);
+const FromExcelToRelationalDatabaseConverter = lazy(() =>
+  import(
+    "./Project/FromExcelToRelationalDatabaseConverter/FromExcelToRelationalDatabaseConverter"
+  )
+);
+const FreeLibrary = lazy(() => import("./Project/FreeLibrary/FreeLibrary"));
+const FreeImages = lazy(() => import("./Project/FreeImages/FreeImages"));
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route path="/free-images">
-          <FreeImages />
-        </Route>
-        <Route path="/free-library">
-          <FreeLibrary />
-        </Route>
-        <Route path="/sunglasses-online-store">
-          <SunglassesOnlineStore />
-        </Route>
-        <Route path="/from-excel-to-relational-database-converter">
-          <FromExcelToRelationalDatabaseConverter />
-        </Route>
-        <Route path="/services">
-          <Main goTo="services" />
-        </Route>
-        <Route path="/about">
-          <Main goTo="about" />
-        </Route>
-        <Route path="/work">
-          <Main goTo="work" />
-        </Route>
-        <Route path="/contact">
-          <Main goTo="contact" />
-        </Route>
-        <Route path="/home">
-          <Main goTo="home" />
-        </Route>
-        <Route path="/">
-          <Main />
-        </Route>
-      </Switch>
-    </Router>
+    <Suspense fallback={<div></div>}>
+      <Router>
+        <Switch>
+          <Route path="/free-images">
+            <FreeImages />
+          </Route>
+          <Route path="/free-library">
+            <FreeLibrary />
+          </Route>
+          <Route path="/sunglasses-online-store">
+            <SunglassesOnlineStore />
+          </Route>
+          <Route path="/from-excel-to-relational-database-converter">
+            <FromExcelToRelationalDatabaseConverter />
+          </Route>
+          <Route path="/services">
+            <Main goTo="services" />
+          </Route>
+          <Route path="/about">
+            <Main goTo="about" />
+          </Route>
+          <Route path="/work">
+            <Main goTo="work" />
+          </Route>
+          <Route path="/contact">
+            <Main goTo="contact" />
+          </Route>
+          <Route path="/home">
+            <Main goTo="home" />
+          </Route>
+          <Route path="/">
+            <Main />
+          </Route>
+        </Switch>
+      </Router>
+    </Suspense>
   );
 }
 export default App;
