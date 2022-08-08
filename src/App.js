bdd68a851e4aee4dd36ca4ae66b057b3dcd46453
jad-nasser <82750984+jad-nasser@users.noreply.github.com>
2022-08-08 11:41:57 +0200

    Optimising website performance

--- src/App.js
+++ src/App.js
@@ -33,9 +33,7 @@ function App() {
           <Route path="/" element={<Main />} />
         </Routes>
       </Router>
-      <Suspense fallback={<div></div>}>
-        <Footer />
-      </Suspense>
+      <Footer />
     </Suspense>
   );
 }
